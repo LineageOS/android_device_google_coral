0c5d9e2c2e29310fc9507b5f8eff14f7b30e0c22
Dan Willemsen <dwillemsen@google.com>
2018-06-21 22:00:35 -0700

    floral: Mark as having overridden commands
    To allow turning these into errors on other devices, and incrementally
    fix these issue, set BUILD_BROKEN_DUP_RULES.
    
    Bug: 77611511
    Test: check out/soong.log
    Change-Id: I3b8f29f0f9e2ad445e72c65e7688b109817dcff3

--- BoardConfigLineage.mk
+++ BoardConfigLineage.mk
@@ -27,6 +27,8 @@ TARGET_KERNEL_CONFIG := floral_defconfig
 TARGET_KERNEL_SOURCE := kernel/google/coral
 TARGET_NEEDS_DTBOIMAGE := true
 
+BUILD_BROKEN_DUP_RULES := true
+
 # Verified Boot
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --set_hashtree_disabled_flag
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 2
