b9842d0841ef05e249dad3a88cd9dcb8c86aed92
Joey <joey@lineageos.org>
2019-01-13 10:21:11 +0100

    floral: Build Trust HAL
    Change-Id: I92706987f8d3094cdfb82e71e5331b7efe69e5df

--- device-lineage.mk
+++ device-lineage.mk
@@ -85,3 +85,7 @@ PRODUCT_PRODUCT_PROPERTIES += ro.opa.eligible_device=true
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
+
+# Trust HAL
+PRODUCT_PACKAGES += \
+    vendor.lineage.trust@1.0-service
