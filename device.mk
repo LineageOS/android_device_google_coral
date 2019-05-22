0b66df6d7094f87371e5baaf2f7231597f5f679d
Rashed Abdel-Tawab <rashed@linux.com>
2019-05-22 15:13:23 -0700

    floral: Add privapp permissions for Google EUICC apps
    Change-Id: I2e7bc09bac103d31132bd6ae646cb2f6d7ba8b19

--- device.mk
+++ device.mk
@@ -956,3 +956,6 @@ PRODUCT_PACKAGES += \
 # EUICC
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.euicc.xml:system/etc/permissions/android.hardware.telephony.euicc.xml
+
+PRODUCT_COPY_FILES += \
+    device/google/coral/permissions/privapp-permissions-aosp-extended.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-aosp-extended.xml
