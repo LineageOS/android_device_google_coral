d2c738d4e6b9ef01b94c1e32c4a1e232b1e8cbd4
razorloves <razorloves@gmail.com>
2019-10-18 00:06:22 -0500

    floral: Move eUICC permissions xml to /product
    All eUICC items were moved to /product in Q.
    
    Also, move it to lineage specific makefile.
    
    Change-Id: I614f86b16f61959c47465863ab1d5d55030209ab

--- device-lineage.mk
+++ device-lineage.mk
@@ -18,6 +18,10 @@
 PRODUCT_PACKAGES += \
     Snap \
 
+# EUICC
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml
+
 # GMS
 WITH_GMS_FI := true
 
