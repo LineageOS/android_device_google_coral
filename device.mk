efc9009611bb963838e06129d96cc1a76aaa2da1
razorloves <razorloves@gmail.com>
2019-10-18 00:06:22 -0500

    floral: Move eUICC permissions xml to /product
    All eUICC items were moved to /product in Q.
    
    Also, move it to lineage specific makefile.
    
    Change-Id: I614f86b16f61959c47465863ab1d5d55030209ab

--- device.mk
+++ device.mk
@@ -952,10 +952,6 @@ endif
 PRODUCT_PACKAGES += \
     misc_writer
 
-# EUICC
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.telephony.euicc.xml:system/etc/permissions/android.hardware.telephony.euicc.xml
-
 PRODUCT_COPY_FILES += \
     device/google/coral/permissions/privapp-permissions-aosp-extended.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-aosp-extended.xml
 
