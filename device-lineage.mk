1111795306d362f67548edd49a71124bd1120d2b
Dan Pasanen <dan.pasanen@gmail.com>
2019-11-01 07:19:07 -0500

    floral: Exclude lineage-sdk overlays from RRO
    * lineage-sdk overlays don't work with RRO
    
    Change-Id: Iff826faf1864388066a488acc9682636b98d1098

--- device-lineage.mk
+++ device-lineage.mk
@@ -25,6 +25,7 @@ PRODUCT_PACKAGES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
+    $(LOCAL_PATH)/overlay-lineage/lineage-sdk \
     $(LOCAL_PATH)/overlay-lineage/packages/apps/Snap \
 
 # Partitions
