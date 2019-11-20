8cb316b248b84ffaea86d25c157bffd1866b9146
Rashed Abdel-Tawab <rashed@linux.com>
2019-11-20 10:22:06 -0500

    floral: Initial Lineage bringup
    Change-Id: Ieddd0460eeb223512b5d3ce9504f6a654d44c0d9

---
+++ lineage_coral.mk
@@ -0,0 +1,26 @@
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2960
+TARGET_SCREEN_WIDTH := 1440
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/google/coral/aosp_coral.mk)
+
+-include device/google/coral/device-lineage.mk
+
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := lineage_coral
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Pixel 4 XL
+TARGET_MANUFACTURER := Google
+PRODUCT_RESTRICT_VENDOR_FILES := false
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=coral \
+    PRIVATE_BUILD_DESC="coral-user 10 QQ1B.200205.002 6084387 release-keys"
+
+BUILD_FINGERPRINT := google/coral/crosshatch:10/QQ1B.200205.002/6084387:user/release-keys
+
+$(call inherit-product-if-exists, vendor/google/coral/coral-vendor.mk)
