b66f9d00015c0e9bdfd42234e13d502d26f84186
Rashed Abdel-Tawab <rashed@linux.com>
2019-11-20 10:09:53 -0500

    floral: device/google/coral-sepolicy -> device/google/coral/sepolicy
    Change-Id: Ib8824df678409a65c315748b7f6b40a29578b774

--- sepolicy/coral-sepolicy.mk
+++ sepolicy/coral-sepolicy.mk
@@ -1,10 +1,10 @@
-PRODUCT_PUBLIC_SEPOLICY_DIRS += device/google/coral-sepolicy/public
-PRODUCT_PRIVATE_SEPOLICY_DIRS += device/google/coral-sepolicy/private
+PRODUCT_PUBLIC_SEPOLICY_DIRS += device/google/coral/sepolicy/public
+PRODUCT_PRIVATE_SEPOLICY_DIRS += device/google/coral/sepolicy/private
 
 # vendors
-BOARD_SEPOLICY_DIRS += device/google/coral-sepolicy/vendor/google
-BOARD_SEPOLICY_DIRS += device/google/coral-sepolicy/vendor/qcom/common
-BOARD_SEPOLICY_DIRS += device/google/coral-sepolicy/vendor/qcom/sm8150
-BOARD_SEPOLICY_DIRS += device/google/coral-sepolicy/vendor/knowles/common
-BOARD_SEPOLICY_DIRS += device/google/coral-sepolicy/tracking_denials
-BOARD_SEPOLICY_DIRS += device/google/coral-sepolicy/vendor/verizon
+BOARD_SEPOLICY_DIRS += device/google/coral/sepolicy/vendor/google
+BOARD_SEPOLICY_DIRS += device/google/coral/sepolicy/vendor/qcom/common
+BOARD_SEPOLICY_DIRS += device/google/coral/sepolicy/vendor/qcom/sm8150
+BOARD_SEPOLICY_DIRS += device/google/coral/sepolicy/vendor/knowles/common
+BOARD_SEPOLICY_DIRS += device/google/coral/sepolicy/tracking_denials
+BOARD_SEPOLICY_DIRS += device/google/coral/sepolicy/vendor/verizon
