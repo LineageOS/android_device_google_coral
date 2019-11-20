d52b225dc42a6c85f11a1b3bce95fd0d98df63e0
Rashed Abdel-Tawab <rashed@linux.com>
2019-11-20 10:22:06 -0500

    floral: Initial Lineage bringup
    Change-Id: Ieddd0460eeb223512b5d3ce9504f6a654d44c0d9

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -25,7 +25,11 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/aosp_flame.mk \
     $(LOCAL_DIR)/aosp_coral_hwasan.mk \
     $(LOCAL_DIR)/aosp_flame_hwasan.mk \
+    $(LOCAL_DIR)/lineage_coral.mk \
+    $(LOCAL_DIR)/lineage_flame.mk
 
 COMMON_LUNCH_CHOICES := \
     aosp_coral-userdebug \
     aosp_flame-userdebug \
+    lineage_coral-userdebug \
+    lineage_flame-userdebug
