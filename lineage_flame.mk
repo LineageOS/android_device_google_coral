7050bb7fb29799055ade5b240f4b6481a0045938
Rashed Abdel-Tawab <rashed@linux.com>
2019-11-20 10:22:06 -0500

    floral: Initial Lineage bringup
    Change-Id: Ieddd0460eeb223512b5d3ce9504f6a654d44c0d9

---
+++ lineage_flame.mk
@@ -0,0 +1,26 @@
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2280
+TARGET_SCREEN_WIDTH := 1080
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/google/coral/aosp_flame.mk)
+
+-include device/google/coral/device-lineage.mk
+
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := lineage_flame
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Pixel 4
+TARGET_MANUFACTURER := Google
+PRODUCT_RESTRICT_VENDOR_FILES := false
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=flame \
+    PRIVATE_BUILD_DESC="flame-user 10 QQ3A.200705.002 6506677 release-keys"
+
+BUILD_FINGERPRINT := google/flame/flame:10/QQ3A.200705.002/6506677:user/release-keys
+
+$(call inherit-product-if-exists, vendor/google/flame/flame-vendor.mk)
