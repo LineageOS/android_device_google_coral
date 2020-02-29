3fc786372cb9d3357a2d925637be061c38793d1e
Rashed Abdel-Tawab <rashed@linux.com>
2020-02-29 21:42:10 +1100

    floral: Build a disabled vbmeta image
    Change-Id: Ie8f1e0929b037b35b5e45c2e31be9253fa0bb097

--- BoardConfig-lineage.mk
+++ BoardConfig-lineage.mk
@@ -24,4 +24,8 @@ TARGET_KERNEL_CONFIG := floral_defconfig
 TARGET_KERNEL_SOURCE := kernel/google/coral
 TARGET_NEEDS_DTBOIMAGE := true
 
+# Verified Boot
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --set_hashtree_disabled_flag
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 2
+
 -include vendor/google/coral/BoardConfigVendor.mk
