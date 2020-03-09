5c294a7469d57aaaa731ba6639e616ecb270b367
P.Adarsh Reddy <padarshr@codeaurora.org>
2020-03-09 12:16:37 +0530

    floral: Add build broken flags in BoardConfig.mk
    BUILD_BROKEN_USES_BUILD_COPY_HEADERS
    
    Change-Id: I7b115f5bb7990c438c595b4b1ff536598786d529

--- BoardConfigLineage.mk
+++ BoardConfigLineage.mk
@@ -27,6 +27,8 @@ TARGET_KERNEL_CONFIG := floral_defconfig
 TARGET_KERNEL_SOURCE := kernel/google/coral
 TARGET_NEEDS_DTBOIMAGE := true
 
+BUILD_BROKEN_USES_BUILD_COPY_HEADERS := true
+
 # Partitions
 AB_OTA_PARTITIONS += \
     vendor
