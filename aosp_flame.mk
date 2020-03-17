2472ed94081bfcbaf9056f667ca4edd0cd2844c1
Eamon Powell <eamonpowell@outlook.com>
2020-03-17 20:50:51 +1100

    flame: Don't copy aosp_excluded_hardware.xml to vendor
    This will only hurt GPS accuracy. Let's just drop this.
    
    Change-Id: I9ae662e47b4bf8d9506f3938d9f4217acf947dc2

--- aosp_flame.mk
+++ aosp_flame.mk
@@ -46,8 +46,8 @@ $(call inherit-product-if-exists, vendor/google_devices/coral/proprietary/device
 $(call inherit-product-if-exists, vendor/google_devices/coral/prebuilts/device-vendor-flame.mk)
 
 # Exclude features that are not available on AOSP devices.
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/aosp_excluded_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/aosp_excluded_hardware.xml
+#PRODUCT_COPY_FILES += \
+#    frameworks/native/data/etc/aosp_excluded_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/aosp_excluded_hardware.xml
 
 # Don't build super.img.
 PRODUCT_BUILD_SUPER_PARTITION := false
