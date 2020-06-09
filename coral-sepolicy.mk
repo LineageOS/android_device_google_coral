f5f035e1b3dc53e7aad011908a2b0eb18cbc3761
Kenny Root <kroot@google.com>
2020-06-09 11:48:32 -0700

    Citadel: move rules to common directory
    Move all the common Citadel rules to a directory where they can all be
    changed simultaneously and avoid accidental version skew between the
    devices.
    
    (cherry picked from commit b498267fc3687b3c90539c7833b1154c5e83d4fb)
    
    Test: build affected devices locally
    Bug: 143330574
    Merged-In: I238f5211ccb606af13fb429134d76eae847a7d8e
    Change-Id: I238f5211ccb606af13fb429134d76eae847a7d8e

--- coral-sepolicy.mk
+++ coral-sepolicy.mk
@@ -8,3 +8,6 @@ BOARD_SEPOLICY_DIRS += device/google/coral-sepolicy/vendor/qcom/sm8150
 BOARD_SEPOLICY_DIRS += device/google/coral-sepolicy/vendor/knowles/common
 BOARD_SEPOLICY_DIRS += device/google/coral-sepolicy/tracking_denials
 BOARD_SEPOLICY_DIRS += device/google/coral-sepolicy/vendor/verizon
+
+# Pixel-wide
+BOARD_SEPOLICY_DIRS += hardware/google/pixel-sepolicy/citadel
