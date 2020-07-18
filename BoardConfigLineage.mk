1dbaaebc8c3fddf3a31ee4c391cf86cf2b700e6f
Dan Pasanen <dan.pasanen@gmail.com>
2020-07-17 20:41:45 -0400

    floral: Build LiveDisplay 2.0 HAL
    * Lets use the common solution, and change our supported
      interfaces while we're at it:
     * Our panel (by default) uses AOSP color profiles.
     * Our panel does support HBM (right now), but elsewhere.
     * Our panel does get along with Color Calibration &
       Reading Mode, which are done SDK side, and require
       no HIDL manifest entries.
     * Our panel does get along with Picture Adjustment
       features, so leave IPictureAdjustment in place.
     * AOSP declares `ro.vendor.display.foss` as `1`, so we
       ought to declare IAdaptiveBacklight as well, as it
       seems to work fine.
    
    Change-Id: Ia96134e0a4bb06269b48671a87991d5e7a53d9ed

--- BoardConfigLineage.mk
+++ BoardConfigLineage.mk
@@ -30,6 +30,7 @@ TARGET_NEEDS_DTBOIMAGE := true
 BUILD_BROKEN_DUP_RULES := true
 
 # Manifests
+DEVICE_MANIFEST_FILE += device/google/coral/lineage_manifest.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += device/google/coral/lineage_compatibility_matrix.xml
 
 # Partitions
@@ -48,6 +49,10 @@ BOARD_SYSTEMIMAGE_PARTITION_RESERVED_SIZE := 1803550720
 endif
 BOARD_SYSTEM_EXTIMAGE_PARTITION_RESERVED_SIZE := 30720000
 BOARD_VENDORIMAGE_PARTITION_RESERVED_SIZE := 30720000
+
+# SELinux
+BOARD_SEPOLICY_DIRS += device/google/coral/sepolicy-lineage/dynamic
+BOARD_SEPOLICY_DIRS += device/google/coral/sepolicy-lineage/vendor
 
 # Verified Boot
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --set_hashtree_disabled_flag
