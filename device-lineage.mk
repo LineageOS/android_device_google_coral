d52d688990ab825f82e46cf35cbfe314f9be32d5
Dan Pasanen <dan.pasanen@gmail.com>
2020-07-17 20:41:45 -0400

    floral: Build LiveDisplay 2.0 HAL
    * Lets use the common solution, and change our supported
      interfaces while we're at it:
     * Our panel (by default) uses AOSP color profiles.
     * Our panel does support HBM (right now), but elsewhere.
     * Our panel does get along with Color Calibration &
       Reading Mode, which are done SDK side, and require
       no HIDL manifest entries.
     * Our panel does get along with Picture Adjustment
       features, so leave IPictureAdjustment in place.
    
    Change-Id: I3a3284d0d3d2fe9c4b3f1b84dce82792c94be186

--- device-lineage.mk
+++ device-lineage.mk
@@ -25,6 +25,10 @@ WITH_GMS_FI := true
 PRODUCT_PACKAGES += \
     hardware.google.light@1.0.vendor \
 
+# LiveDisplay
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay@2.0-service-sdm \
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
