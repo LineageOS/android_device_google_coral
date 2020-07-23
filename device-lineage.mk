2fe2f93788e4dc7621d7a7e827c9f230d9a937bb
Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
2020-07-22 23:22:12 -0400

    floral: Build a lights HAL dependency
    * The 32-bit version of version 1.0 isn't compiled by
      default, everything else was.
    
    Change-Id: Ieed197e27cf30997c158bc6a8c47eac9428fe3fc

--- device-lineage.mk
+++ device-lineage.mk
@@ -21,6 +21,10 @@ PRODUCT_PACKAGES += \
 # GMS
 WITH_GMS_FI := true
 
+# Lights
+PRODUCT_PACKAGES += \
+    hardware.google.light@1.0.vendor \
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
