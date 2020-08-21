9988baff89e2e6c54277a5b8b529ba13baae6f3a
Nolen Johnson <johnsonnolen@gmail.com>
2020-08-21 02:35:01 -0400

    Revert "floral: Exclude Snap overlays from RRO"
    This reverts commit 1a6d457521d20d49322ec6d27d57102618246894.
    
    Change-Id: I4fc6cb37d94dcfe134f36188b781782f6795a3ef

--- device-lineage.mk
+++ device-lineage.mk
@@ -37,7 +37,6 @@ PRODUCT_PACKAGES += \
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
     $(LOCAL_PATH)/overlay-lineage/lineage-sdk \
-    $(LOCAL_PATH)/overlay-lineage/packages/apps/Snap \
 
 # Partitions
 AB_OTA_PARTITIONS += \
