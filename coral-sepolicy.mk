3798e2027103a9ef18084c56b5508dc6ad9bc2dd
George Chang <georgekgchang@google.com>
2020-09-18 20:09:29 +0800

    Update ST NFC/SecureElement policies
    Bug: 168875298
    Bug: 160672745
    Test: check no avc denial for nfc
    Merged-In: I51059a5a8f4afbb41505d1ed826c6aea8027894d
    Change-Id: I51059a5a8f4afbb41505d1ed826c6aea8027894d

--- coral-sepolicy.mk
+++ coral-sepolicy.mk
@@ -7,4 +7,5 @@ BOARD_SEPOLICY_DIRS += device/google/coral-sepolicy/vendor/qcom/common
 BOARD_SEPOLICY_DIRS += device/google/coral-sepolicy/vendor/qcom/sm8150
 BOARD_SEPOLICY_DIRS += device/google/coral-sepolicy/vendor/knowles/common
 BOARD_SEPOLICY_DIRS += device/google/coral-sepolicy/tracking_denials
+BOARD_SEPOLICY_DIRS += device/google/coral-sepolicy/vendor/st
 BOARD_SEPOLICY_DIRS += device/google/coral-sepolicy/vendor/verizon
