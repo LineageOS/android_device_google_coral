ae172bfdeaf4baf6aafa0aae80f89d6cf486349e
Aayush Gupta <aayushgupta219@gmail.com>
2020-10-22 13:02:38 +0530

    floral: Switch to BOARD_VENDOR_SEPOLICY_DIRS
    - BOARD_SEPOLICY_DIRS is deprecated and gives compile-time
      errors when used in unison with a device using BOARD_VENDOR_SEPOLICY_DIRS
    
    Ref:
    [0]: https://github.com/LineageOS/android_system_sepolicy/commit/ec3ac470a98342c13c1fec8d46433c73b08531be
    
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>
    Change-Id: Icefb062cc8cdef532b4310684d9a66afe97e49c4

--- sepolicy/coral-sepolicy.mk
+++ sepolicy/coral-sepolicy.mk
@@ -8,23 +8,23 @@ PRODUCT_PUBLIC_SEPOLICY_DIRS += device/google/coral/sepolicy/public
 PRODUCT_PRIVATE_SEPOLICY_DIRS += device/google/coral/sepolicy/private
 
 # vendors
-BOARD_SEPOLICY_DIRS += device/google/coral/sepolicy/vendor/google
-BOARD_SEPOLICY_DIRS += device/google/coral/sepolicy/vendor/qcom/common
-BOARD_SEPOLICY_DIRS += device/google/coral/sepolicy/vendor/qcom/sm8150
-BOARD_SEPOLICY_DIRS += device/google/coral/sepolicy/vendor/knowles/common
-BOARD_SEPOLICY_DIRS += device/google/coral/sepolicy/tracking_denials
-BOARD_SEPOLICY_DIRS += device/google/coral/sepolicy/vendor/st
-BOARD_SEPOLICY_DIRS += device/google/coral/sepolicy/vendor/verizon
+BOARD_VENDOR_SEPOLICY_DIRS += device/google/coral/sepolicy/vendor/google
+BOARD_VENDOR_SEPOLICY_DIRS += device/google/coral/sepolicy/vendor/qcom/common
+BOARD_VENDOR_SEPOLICY_DIRS += device/google/coral/sepolicy/vendor/qcom/sm8150
+BOARD_VENDOR_SEPOLICY_DIRS += device/google/coral/sepolicy/vendor/knowles/common
+BOARD_VENDOR_SEPOLICY_DIRS += device/google/coral/sepolicy/tracking_denials
+BOARD_VENDOR_SEPOLICY_DIRS += device/google/coral/sepolicy/vendor/st
+BOARD_VENDOR_SEPOLICY_DIRS += device/google/coral/sepolicy/vendor/verizon
 
 # Pixel-wide
-BOARD_SEPOLICY_DIRS += hardware/google/pixel-sepolicy/confirmationui_hal
-BOARD_SEPOLICY_DIRS += hardware/google/pixel-sepolicy/powerstats
-BOARD_SEPOLICY_DIRS += hardware/google/pixel-sepolicy/ramdump/common
+BOARD_VENDOR_SEPOLICY_DIRS += hardware/google/pixel-sepolicy/confirmationui_hal
+BOARD_VENDOR_SEPOLICY_DIRS += hardware/google/pixel-sepolicy/powerstats
+BOARD_VENDOR_SEPOLICY_DIRS += hardware/google/pixel-sepolicy/ramdump/common
 
 # system_ext
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += device/google/coral/sepolicy/system_ext/public
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += device/google/coral/sepolicy/system_ext/private
 
 # Lineage-specific
-BOARD_SEPOLICY_DIRS += device/google/coral/sepolicy/dynamic/lineage
-BOARD_SEPOLICY_DIRS += device/google/coral/sepolicy/vendor/lineage
+BOARD_VENDOR_SEPOLICY_DIRS += device/google/coral/sepolicy/dynamic/lineage
+BOARD_VENDOR_SEPOLICY_DIRS += device/google/coral/sepolicy/vendor/lineage
