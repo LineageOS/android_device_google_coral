e026a0dcebddc336d7003d8cf1871b3d471cb71a
Eamon Powell <eamonpowell@outlook.com>
2021-08-07 13:06:33 +1000

    floral: RQ3A.210805.001.A1

--- lineage_flame.mk
+++ lineage_flame.mk
@@ -17,8 +17,8 @@ PRODUCT_BRAND := google
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=flame \
-    PRIVATE_BUILD_DESC="flame-user 11 RQ3A.210705.001 7380771 release-keys"
+    PRIVATE_BUILD_DESC="flame-user 11 RQ3A.210805.001.A1 7474174 release-keys"
 
-BUILD_FINGERPRINT := google/flame/flame:11/RQ3A.210705.001/7380771:user/release-keys
+BUILD_FINGERPRINT := google/flame/flame:11/RQ3A.210805.001.A1/7474174:user/release-keys
 
 $(call inherit-product, vendor/google/flame/flame-vendor.mk)
