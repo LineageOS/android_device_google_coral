bcef7de6627c77af878ebb634c018128329f8bef
Eamon Powell <eamonpowell@outlook.com>
2022-04-27 14:28:00 +1000

    floral: Use ThinLTO on unofficial builds
    Currently building with full LTO enabled on unofficial builds can
    lead to kernels building for hours and failing if the build system
    lacks the memory necessary. Switch to thinLTO on unofficial builds
    to remedy this.
    
    Change-Id: I2c052a35891579f652bbe4c59f081220e85c7d3d

--- BoardConfigLineage.mk
+++ BoardConfigLineage.mk
@@ -19,6 +19,12 @@ TARGET_KERNEL_CONFIG := floral_defconfig
 TARGET_KERNEL_SOURCE := kernel/google/msm-4.14
 TARGET_NEEDS_DTBOIMAGE := true
 
+# Use ThinLTO on unofficial builds
+ifneq ($(filter UNOFFICIAL,$(LINEAGE_BUILDTYPE)),)
+    KERNEL_LTO := thin
+endif
+
+
 # Manifests
 DEVICE_MANIFEST_FILE += device/google/coral/lineage_manifest.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/lineage/config/device_framework_matrix.xml
