9bfb655489bf3be4d811c2083851426e93c5c0ea
Roshan Pius <rpius@google.com>
2023-11-04 18:59:20 +0000

    Revert "coral(nfc): Modify NFC overlays for NFC apex"
    Revert submission 2802548-nfc_apex
    
    Reason for revert: Causing bootloop with signed builds
    
    Reverted changes: /q/submissionid:2802548-nfc_apex
    
    Bug: 309117462
    Test: Compiles, device boots up
    Merged-In: I482f23274fd752d196e76568c12b5d6eb5d5212e
    Change-Id: I482f23274fd752d196e76568c12b5d6eb5d5212e

--- device.mk
+++ device.mk
@@ -450,8 +450,7 @@ PRODUCT_PACKAGES += \
     Tag \
     SecureElement \
     android.hardware.nfc-service.st \
-    android.hardware.secure_element@1.0-service.st \
-    NfcOverlayCoral
+    android.hardware.secure_element@1.0-service.st
 
 PRODUCT_COPY_FILES += \
     device/google/coral/nfc/com.google.hardware.pixel.japan.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/sku_G020N/com.google.hardware.pixel.japan.xml \
