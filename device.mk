89bb6d45273c934a93996e48b6399a2dde792b13
Roshan Pius <rpius@google.com>
2023-12-06 21:31:18 +0000

    Revert^2 "coral(nfc): Modify NFC overlays for NFC apex"
    6b92e954f075ec22e678cda5e655a7f77daa8eb4
    
    Bug: 303286040
    Test: Compiles
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:b1f3f994e253788582b7d5b48b9fbd8294e62480)
    Merged-In: I80a8529cc2ed8755029471e048acc5cbfc955e56
    Change-Id: I80a8529cc2ed8755029471e048acc5cbfc955e56

--- device.mk
+++ device.mk
@@ -450,7 +450,8 @@ PRODUCT_PACKAGES += \
     Tag \
     SecureElement \
     android.hardware.nfc-service.st \
-    android.hardware.secure_element@1.0-service.st
+    android.hardware.secure_element@1.0-service.st \
+    NfcOverlayCoral
 
 PRODUCT_COPY_FILES += \
     device/google/coral/nfc/com.google.hardware.pixel.japan.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/sku_G020N/com.google.hardware.pixel.japan.xml \
