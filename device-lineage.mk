7ff2b30074a18c5113e2e5ce08f3aad749259d6d
Michael Bestas <mikeioannina@gmail.com>
2024-06-13 19:16:13 +0300

    floral: Build missing libraries for 14 QPR3
    Change-Id: I6a84526ca1e9f4e5140c8068646433cab3fe85b2

--- device-lineage.mk
+++ device-lineage.mk
@@ -58,6 +58,10 @@ include hardware/google/pixel/touch/device.mk
 
 # Build necessary packages for system_ext
 
+# Audio
+PRODUCT_PACKAGES += \
+    libaudioroute.vendor
+
 # Display
 PRODUCT_PACKAGES += \
     vendor.display.config@1.0 \
@@ -167,7 +171,8 @@ PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0.vendor:64 \
     libwifi-hal:64 \
     libwifi-hal-ctrl:64 \
-    libwifi-hal-qcom
+    libwifi-hal-qcom \
+    libwifi-system-iface.vendor:64
 
 # Misc interfaces
 PRODUCT_PACKAGES += \
@@ -208,6 +213,13 @@ PRODUCT_PACKAGES += \
     android.hardware.thermal@2.0.vendor:64 \
     android.hardware.weaver@1.0.vendor:64 \
     android.system.net.netd@1.1.vendor:64
+
+# Misc
+PRODUCT_PACKAGES += \
+    libcrypto_utils.vendor:64 \
+    libpng.vendor \
+    libsqlite.vendor \
+    libssl.vendor:32
 
 # Properties
 TARGET_VENDOR_PROP := $(LOCAL_PATH)/vendor.prop
