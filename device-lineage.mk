9eb92243e637e1a0e7b1061922e1276cff10edb6
Michael Bestas <mkbestas@lineageos.org>
2025-01-01 03:50:53 +0200

    floral: Update display flags & packages
    Required after display HAL blueprint conversion
    
    Change-Id: Ifc4e10703d3424607b9dc8bcc8e417d1c88cc2f3

--- device-lineage.mk
+++ device-lineage.mk
@@ -79,6 +79,9 @@ PRODUCT_PACKAGES += \
     disable_configstore
 
 # Display
+$(call soong_config_set,qtidisplay,default,true)
+$(call soong_config_set,qtidisplay,drmpp,true)
+$(call soong_config_set,qtidisplay,gralloc4,true)
 $(call soong_config_set,qtidisplay,gralloc_handle_has_reserved_size,true)
 
 # GPS
