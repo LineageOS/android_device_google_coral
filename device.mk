e5e4d0c087f54bf44cc01bc7c4e686177fe19c13
Michael Bestas <mkbestas@lineageos.org>
2025-01-06 11:41:20 +0200

    floral: Remove non existent soong namespaces
    Change-Id: If0b86441d47da127e37aeed6fcd6cbfff656e3f8

--- device.mk
+++ device.mk
@@ -19,56 +19,14 @@ LOCAL_PATH := device/google/coral
 PRODUCT_VENDOR_MOVE_ENABLED := true
 
 PRODUCT_SOONG_NAMESPACES += \
+    device/google/coral \
     hardware/google/interfaces \
     hardware/google/pixel \
-    device/google/coral \
     hardware/qcom/sm8150/display \
     hardware/qcom/sm8150/gps \
     hardware/qcom/wlan/legacy \
     system/chre/host/hal_generic \
-    vendor/google/airbrush/floral \
-    vendor/google/biometrics/face/florence \
-    vendor/google/camera \
-    vendor/google/darwinn \
-    vendor/google/tools/power-anomaly-qcril \
-    vendor/google_devices/common/proprietary/confirmatioui_hal \
-    vendor/google_nos/host/android \
-    vendor/qcom/opensource/data-ipa-cfg-mgr-legacy-um \
-    vendor/qcom/sm8150 \
-    vendor/qcom/sm8150/proprietary/commonsys/telephony-apps/DataStatusNotification \
-    vendor/qcom/sm8150/proprietary/gps \
-    vendor/qcom/sm8150/proprietary/qmi \
-    vendor/qcom/sm8150/codeaurora/location \
-    vendor/google/interfaces \
-    vendor/google_nos/test/system-test-harness
-
-# Include sensors soong namespace
-PRODUCT_SOONG_NAMESPACES += \
-    vendor/qcom/sensors \
-    vendor/google/tools/sensors
-
-# Single vendor RIL/Telephony/data with SM7250
-DEVICE_USES_SM7250_QCRIL_TELEPHONY := true
-
-ifeq ($(DEVICE_USES_SM7250_QCRIL_TELEPHONY), true)
-  PRODUCT_SOONG_NAMESPACES += \
-      vendor/qcom/sm7250/codeaurora/commonsys/telephony/ims/ims-ext-common \
-      vendor/qcom/sm7250/codeaurora/dataservices/rmnetctl \
-      vendor/qcom/sm7250/proprietary/commonsys/qcrilOemHook \
-      vendor/qcom/sm7250/proprietary/commonsys/telephony-apps/ims \
-      vendor/qcom/sm7250/proprietary/commonsys/telephony-apps/QtiTelephonyService \
-      vendor/qcom/sm7250/proprietary/commonsys/telephony-apps/xdivert \
-      vendor/qcom/sm7250/proprietary/qcril-data-hal \
-      vendor/qcom/sm7250/proprietary/qcril-hal \
-      vendor/qcom/sm7250/proprietary/data
-else
-  $(warning DEVICE_USES_SM7250_QCRIL_TELEPHONY is disabled)
-
-  PRODUCT_SOONG_NAMESPACES += \
-      vendor/qcom/sm8150/codeaurora/telephony/ims \
-      vendor/qcom/sm8150/proprietary/qcril-data-hal \
-      vendor/qcom/sm8150/proprietary/qcril-hal
-endif
+    vendor/qcom/opensource/data-ipa-cfg-mgr-legacy-um
 
 PRODUCT_PROPERTY_OVERRIDES += \
     keyguard.no_require_sim=true
@@ -406,9 +364,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.bluetooth.emb_wp_mode=false \
     ro.vendor.bluetooth.wipower=false
-
-# Bluetooth ftmdaemon needs libbt-hidlclient.so
-PRODUCT_SOONG_NAMESPACES += vendor/qcom/proprietary/bluetooth/hidl_client
 
 # DRM HAL
 PRODUCT_PACKAGES += \
