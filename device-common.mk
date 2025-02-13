20c6d99e9e9cd78ed6b9d015b6e9ef62de6c8d49
Yumi Yukimura <me.cafebabe@gmail.com>
2025-02-13 15:40:58 +0800

    floral: Disable UFFD GC via `OVERRIDE_ENABLE_UFFD_GC`
    * Our kernel (4.14) does not support features needed for UFFD GC.
      Leaving it enabled results in long waiting time on the first boot.
    
    Change-Id: Ia8a17467e13d343c03ee29e514994946e7e84985

--- device-common.mk
+++ device-common.mk
@@ -124,3 +124,6 @@ PRODUCT_ENFORCE_PRODUCT_PARTITION_INTERFACE := true
 PRODUCT_PACKAGES += \
     libprotobuf-cpp-full-3.9.1-vendorcompat \
     libprotobuf-cpp-lite-3.9.1-vendorcompat
+
+# UFFD GC
+OVERRIDE_ENABLE_UFFD_GC := false
