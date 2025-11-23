236559a18ebfa1cc4777ef94605e2595065f8eb8
Yumi Yukimura <me.cafebabe@gmail.com>
2025-11-24 03:08:29 +0800

    floral: Update some soong config variables to bool type
    Change-Id: Ice4a8e2cc6d756a743116b46299cf833a0e78c57

--- device-common.mk
+++ device-common.mk
@@ -135,7 +135,7 @@ PRODUCT_PACKAGES += \
     android.hardware.boot-service.qti \
     android.hardware.boot-service.qti.recovery
 
-$(call soong_config_set,QTI_GPT_UTILS,USE_BSG_FRAMEWORK,false)
+$(call soong_config_bool,QTI_GPT_UTILS,USE_BSG_FRAMEWORK,false)
 
 # Camera
 PRODUCT_PACKAGES += \
@@ -296,7 +296,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay-service.sdm
 
-$(call soong_config_set,livedisplay_sdm,enable_dm,false)
+$(call soong_config_bool,livedisplay_sdm,enable_dm,false)
 
 # LMKD
 TARGET_LMKD_STATS_LOG := true
