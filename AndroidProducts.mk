296cd53d4d5725d58d1512c64819f5379a972427
Nolen Johnson <johnsonnolen@gmail.com>
2025-11-25 17:59:26 -0500

    floral: Drop hwasan support
    Change-Id: I4ca26342f9ffb7b1bfe8b4fba9bdae1ebdfe5ca1

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -13,8 +13,6 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/aosp_coral.mk \
     $(LOCAL_DIR)/aosp_flame.mk \
-    $(LOCAL_DIR)/aosp_coral_hwasan.mk \
-    $(LOCAL_DIR)/aosp_flame_hwasan.mk \
     $(LOCAL_DIR)/lineage_coral.mk \
     $(LOCAL_DIR)/lineage_flame.mk
 
