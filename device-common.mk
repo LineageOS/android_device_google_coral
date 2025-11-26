1efbabd3e67a39181cb877a5b48f68013dbd5c61
Nolen Johnson <johnsonnolen@gmail.com>
2025-11-26 02:34:26 -0500

    floral: Drop dead includes/flags
    * Either Google proprietary or QCOM BSP related.
    * Also make libperfmgr mandatory.
    
    Change-Id: I3bcff31f716d8c0f63c6722be573c09b450e392f

--- device-common.mk
+++ device-common.mk
@@ -209,9 +209,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.dumpstate@1.1-service.coral
 
-# ECO Service
-QC2_HAVE_ECO_SERVICE := true
-
 # Elmyra
 PRODUCT_PACKAGES += \
     ElmyraService
@@ -417,7 +414,7 @@ PRODUCT_COPY_FILES += \
 include hardware/google/pixel/PixelLogger/PixelLogger.mk
 
 # Power
--include hardware/google/pixel/power-libperfmgr/aidl/device.mk
+include hardware/google/pixel/power-libperfmgr/aidl/device.mk
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/power/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
@@ -441,11 +438,7 @@ PRODUCT_PACKAGES += \
     RcsService
 
 # RIL
--include vendor/google/build/soong/soong_config_namespace/qcril_oemhook.mk
-
-BOARD_USES_QCNE := true
 ENABLE_VENDOR_RIL_SERVICE := true
-USE_QCRIL_OEMHOOK := true
 
 PRODUCT_PACKAGES += \
     qti-telephony-hidl-wrapper \
@@ -454,10 +447,6 @@ PRODUCT_PACKAGES += \
     qti_telephony_hidl_wrapper_prd.xml \
     qti-telephony-utils \
     qti_telephony_utils.xml
-
-# Security
--include vendor/qcom/sm8150/proprietary/securemsm/config/keymaster_vendor_proprietary_board.mk
--include vendor/qcom/sm8150/proprietary/securemsm/config/keymaster_vendor_proprietary_product.mk
 
 # Security Patch Levels
 BOOT_SECURITY_PATCH = 2022-10-05
