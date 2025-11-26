4a674a9ee3a0c00087b6ddbf4c81ce666fa93467
Nolen Johnson <johnsonnolen@gmail.com>
2025-11-26 02:41:18 -0500

    floral: Move sound trigger soong_config_set to product makefile
    * soong_config_set in BoardConfig is a Lineage thing, no need to
      use it here.
    
    Change-Id: Iaa9dc7185992f2745d4189f113918de25279bcf4

--- BoardConfig-common.mk
+++ BoardConfig-common.mk
@@ -124,7 +124,6 @@ include $(DEVICE_COMMON_PATH)/sepolicy/coral-sepolicy.mk
 
 # Sound Trigger
 BOARD_SUPPORTS_SOUND_TRIGGER := true
-$(call soong_config_set,knowles,use_sound_trigger_hal,iaxxx)
 
 # Testing related defines
 BOARD_PERFSETUP_SCRIPT := platform_testing/scripts/perf-setup/c2f2-setup.sh
