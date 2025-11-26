11c173ff293e521cc1f7162921ff62cef815e70c
Nolen Johnson <johnsonnolen@gmail.com>
2025-11-26 02:32:22 -0500

    floral: Organize and alphabetize makefiles
    Change-Id: I6af9e98383131d0fca1489f87e9ea95efefd4add

--- coral/device.mk
+++ coral/device.mk
@@ -8,27 +8,29 @@ PRODUCT_HARDWARE := coral
 
 include device/google/coral/device-common.mk
 
-DEVICE_PACKAGE_OVERLAYS += device/google/coral/coral/overlay
-
-# Audio XMLs for coral
+# Audio
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_policy_volumes_coral.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml
 
-# Bluetooth Tx power caps for coral
+# Bluetooth
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/bluetooth/bluetooth_power_limits_coral.csv:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth_power_limits.csv
+
+# Boot Animation
+TARGET_SCREEN_HEIGHT := 3040
+TARGET_SCREEN_WIDTH := 1440
 
 # CHRE
 $(call soong_config_set,chre,chre_daemon_dsp_library,//vendor/google/coral:libsdsprpc)
 
-# Display config
+# Display
 PRODUCT_COPY_FILES += \
     device/google/coral/configs/displayconfig/display_19260504575090817.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/displayconfig/display_19260504575090817.xml
 
-# Settings overlay packages for regulatory_info
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/coral/coral/overlay
+
 PRODUCT_PACKAGES += \
     SettingsOverlayG020J \
     SettingsOverlayG020P \
-    SettingsOverlayG020Q \
-
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/coral/overlay-lineage
+    SettingsOverlayG020Q
