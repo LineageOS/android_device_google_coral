bde364fdda84d3699bfb79401bf8eb0295e8b93c
Nolen Johnson <johnsonnolen@gmail.com>
2025-11-26 02:32:22 -0500

    floral: Organize and alphabetize makefiles
    Change-Id: I6af9e98383131d0fca1489f87e9ea95efefd4add

--- flame/BoardConfig.mk
+++ flame/BoardConfig.mk
@@ -4,8 +4,13 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Bootloader
 TARGET_BOOTLOADER_BOARD_NAME := flame
+
+# Display
 TARGET_SCREEN_DENSITY := 440
+
+# Recovery
 TARGET_RECOVERY_UI_MARGIN_HEIGHT := 165
 
 include device/google/coral/BoardConfig-common.mk
