02d49eab4762a4b830bbf75f7fdee7136003606a
Nolen Johnson <johnsonnolen@gmail.com>
2025-11-25 23:21:41 -0500

    floral: Drop AOSP build support
    * We rely on a bunch of lineage modified repos, and other such hacks.
    * Also drop AOSP ownership tracking/Gerrit config files.
    
    Change-Id: Iaba878e066815d6a639b95623b978fb4df715daf

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,17 +1,8 @@
 #
-# Copyright 2018 The Android Open Source Project
+# Copyright (C) 2018 The Android Open-Source Project
+# Copyright (C) The LineageOS Project
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# SPDX-License-Identifier: Apache-2.0
 #
 
 #
@@ -21,13 +12,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_coral.mk \
-    $(LOCAL_DIR)/aosp_flame.mk \
     $(LOCAL_DIR)/lineage_coral.mk \
     $(LOCAL_DIR)/lineage_flame.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_coral-trunk_staging-userdebug \
-    aosp_flame-trunk_staging-userdebug \
     lineage_coral-trunk_staging-userdebug \
     lineage_flame-trunk_staging-userdebug
