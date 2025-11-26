7f8812bf79333b17ad4cb45b817ce38b4971ab14
Nolen Johnson <johnsonnolen@gmail.com>
2025-11-25 23:27:29 -0500

    floral: Drop incomplete automotive build support
    * We are missing the repo these makefiles live in.
    
    Change-Id: I751cffa53c3999ce2e2a8fd307f826fea664192b

--- coral/BoardConfig.mk
+++ coral/BoardConfig.mk
@@ -1,30 +1,13 @@
 #
 # Copyright (C) 2018 The Android Open-Source Project
+# Copyright (C) The LineageOS Project
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# SPDX-License-Identifier: Apache-2.0
 #
 
-ifdef AUTOMOTIVE_PRODUCT_PATH
-  #RBC# include_top device/google/auto_tcu
-  #RBC# include_top device/google/pixel_tcu
-  #RBC# include_top device/google_car
-  include device/$(AUTOMOTIVE_PRODUCT_PATH)/BoardConfig.mk
-else
-  TARGET_BOOTLOADER_BOARD_NAME := coral
-  TARGET_SCREEN_DENSITY := 560
-  TARGET_RECOVERY_UI_MARGIN_HEIGHT := 165
+TARGET_BOOTLOADER_BOARD_NAME := coral
+TARGET_SCREEN_DENSITY := 560
+TARGET_RECOVERY_UI_MARGIN_HEIGHT := 165
 
-  include device/google/coral/BoardConfig-common.mk
-endif
-
+include device/google/coral/BoardConfig-common.mk
 include vendor/google/coral/BoardConfigVendor.mk
