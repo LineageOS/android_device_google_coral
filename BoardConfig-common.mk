aa7e96fbc72b5b2e11e5980700681ff20477d7f3
Nolen Johnson <johnsonnolen@gmail.com>
2025-11-26 02:32:22 -0500

    floral: Organize and alphabetize makefiles
    Change-Id: I6af9e98383131d0fca1489f87e9ea95efefd4add

--- BoardConfig-common.mk
+++ BoardConfig-common.mk
@@ -4,12 +4,26 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+DEVICE_COMMON_PATH := device/google/coral
+
+# Includes
 include build/make/target/board/BoardConfigMainlineCommon.mk
 include build/make/target/board/BoardConfigPixelCommon.mk
 
-TARGET_BOARD_PLATFORM := msmnile
-TARGET_BOARD_INFO_FILE := device/google/coral/board-info.txt
+# A/B
+AB_OTA_UPDATER := true
 
+AB_OTA_PARTITIONS += \
+    boot \
+    dtbo \
+    product \
+    system \
+    system_ext \
+    vbmeta \
+    vbmeta_system \
+    vendor
+
+# Architecture
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_ABI := arm64-v8a
@@ -24,150 +38,114 @@ TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := generic
 TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a76
 
-BOARD_KERNEL_CMDLINE += console=ttyMSM0,115200n8 androidboot.console=ttyMSM0 printk.devkmsg=on
-BOARD_KERNEL_CMDLINE += msm_rtb.filter=0x237
-BOARD_KERNEL_CMDLINE += ehci-hcd.park=3
-BOARD_KERNEL_CMDLINE += service_locator.enable=1
-BOARD_KERNEL_CMDLINE += androidboot.memcg=1 cgroup.memory=nokmem
-BOARD_KERNEL_CMDLINE += usbcore.autosuspend=7
-BOARD_KERNEL_CMDLINE += androidboot.usbcontroller=a600000.dwc3 swiotlb=2048
-BOARD_KERNEL_CMDLINE += androidboot.boot_devices=soc/1d84000.ufshc
-BOARD_KERNEL_CMDLINE += loop.max_part=7
-BOARD_KERNEL_CMDLINE += loop.hw_queue_depth=31
-BOARD_KERNEL_CMDLINE += cgroup_disable=pressure
+# Assertations
+TARGET_BOARD_INFO_FILE := $(DEVICE_COMMON_PATH)/board-info.txt
 
-BOARD_KERNEL_BASE        := 0x00000000
-BOARD_KERNEL_PAGESIZE    := 4096
-
-BOARD_INCLUDE_DTB_IN_BOOTIMG := true
-BOARD_BOOT_HEADER_VERSION := 2
-BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
-
-# DTBO partition definitions
-BOARD_DTBOIMG_PARTITION_SIZE := 8388608
-
-BOARD_USES_RECOVERY_AS_BOOT := true
-BOARD_USES_METADATA_PARTITION := true
-
-AB_OTA_UPDATER := true
-
-AB_OTA_PARTITIONS += \
-    boot \
-    system \
-    vbmeta \
-    dtbo \
-    product \
-    system_ext \
-    vbmeta_system \
-    vendor
-
-# Partitions (listed in the file) to be wiped under recovery.
-TARGET_RECOVERY_WIPE := device/google/coral/init/recovery.wipe
-TARGET_RECOVERY_FSTAB := device/google/coral/init/fstab.hardware
-TARGET_RECOVERY_PIXEL_FORMAT := RGBX_8888
-TARGET_RECOVERY_UI_LIB := \
-    //hardware/google/pixel/recovery:librecovery_ui_pixel \
-    libfstab
-
-# Enable chain partition for system.
-BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
-BOARD_AVB_VBMETA_SYSTEM := system system_ext product
-BOARD_AVB_VBMETA_SYSTEM_KEY_PATH := external/avb/test/data/testkey_rsa2048.pem
-BOARD_AVB_VBMETA_SYSTEM_ALGORITHM := SHA256_RSA2048
-BOARD_AVB_VBMETA_SYSTEM_ROLLBACK_INDEX := $(PLATFORM_SECURITY_PATCH_TIMESTAMP)
-BOARD_AVB_VBMETA_SYSTEM_ROLLBACK_INDEX_LOCATION := 1
-
-
-# product.img
-BOARD_PRODUCTIMAGE_FILE_SYSTEM_TYPE := ext4
-
-# system_ext.img
-BOARD_SYSTEM_EXTIMAGE_FILE_SYSTEM_TYPE := ext4
-
-# userdata.img
-TARGET_USERIMAGES_USE_F2FS := true
-
-# persist.img
-BOARD_PERSISTIMAGE_PARTITION_SIZE := 33554432
-BOARD_PERSISTIMAGE_FILE_SYSTEM_TYPE := ext4
-
-# vendor.img
-BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
-
-# boot.img
-BOARD_BOOTIMAGE_PARTITION_SIZE := 0x04000000
-
-BOARD_FLASH_BLOCK_SIZE := 131072
-
-BOARD_ROOT_EXTRA_SYMLINKS := /mnt/vendor/persist:/persist
-
-include device/google/coral/sepolicy/coral-sepolicy.mk
-
-TARGET_FS_CONFIG_GEN := device/google/coral/config.fs
+# Audio
+BOARD_USES_ALSA_AUDIO := true
 
 # Camera
 TARGET_USES_ION := true
+
+# CHRE
+CHRE_DAEMON_ENABLED := true
+CHRE_DAEMON_LOAD_INTO_SENSORSPD := true
+CHRE_DAEMON_LPMA_ENABLED := true
+
+# Filesystem
+TARGET_FS_CONFIG_GEN := $(DEVICE_COMMON_PATH)/config.fs
 
 # GPS
 BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := default
 BOARD_VENDOR_QCOM_LOC_PDK_FEATURE_SET := true
 
-# CHRE
-CHRE_DAEMON_ENABLED := true
-CHRE_DAEMON_LPMA_ENABLED := true
-CHRE_DAEMON_LOAD_INTO_SENSORSPD := true
-
-# wlan
-BOARD_WLAN_DEVICE := qcwcn
-BOARD_WPA_SUPPLICANT_DRIVER := NL80211
-BOARD_HOSTAPD_DRIVER := NL80211
-WIFI_DRIVER_DEFAULT := qca_cld3
-WPA_SUPPLICANT_VERSION := VER_0_8_X
-BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
-BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
-WIFI_HIDL_FEATURE_AWARE := true
-WIFI_HIDL_FEATURE_DUAL_INTERFACE:= true
-WIFI_FEATURE_WIFI_EXT_HAL := true
-WIFI_FEATURE_IMU_DETECTION := true
-WIFI_HIDL_UNIFIED_SUPPLICANT_SERVICE_RC_ENTRY := true
-
-# Audio
-BOARD_USES_ALSA_AUDIO := true
-
-# Sound Trigger
-BOARD_SUPPORTS_SOUND_TRIGGER := true
-$(call soong_config_set,knowles,use_sound_trigger_hal,iaxxx)
-
-# Vendor Interface Manifest
+# HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/lineage/config/device_framework_matrix.xml
-DEVICE_MANIFEST_FILE := device/google/coral/manifest.xml
-DEVICE_MATRIX_FILE := device/google/coral/compatibility_matrix.xml
-# Intall device framework compatibility matrix to product partition
-DEVICE_PRODUCT_COMPATIBILITY_MATRIX_FILE := device/google/coral/device_framework_matrix_product.xml
-
-BOARD_SUPER_PARTITION_SIZE := 9755951104
-BOARD_SUPER_PARTITION_GROUPS := google_dynamic_partitions
-BOARD_GOOGLE_DYNAMIC_PARTITIONS_PARTITION_LIST := \
-    system \
-    vendor \
-    product \
-    system_ext
-
-#BOARD_GOOGLE_DYNAMIC_PARTITIONS_SIZE is set to BOARD_SUPER_PARTITION_SIZE / 2 - 4MB
-BOARD_GOOGLE_DYNAMIC_PARTITIONS_SIZE := 4873781248
-
-# Set error limit to BOARD_SUPER_PARTITON_SIZE - 100MB
-BOARD_SUPER_PARTITION_ERROR_LIMIT := 9651093504
-
-# Testing related defines
-BOARD_PERFSETUP_SCRIPT := platform_testing/scripts/perf-setup/c2f2-setup.sh
+DEVICE_MANIFEST_FILE := $(DEVICE_COMMON_PATH)/manifest.xml
+DEVICE_MATRIX_FILE := $(DEVICE_COMMON_PATH)/compatibility_matrix.xml
+DEVICE_PRODUCT_COMPATIBILITY_MATRIX_FILE := $(DEVICE_COMMON_PATH)/device_framework_matrix_product.xml
 
 # Kernel
+BOARD_BOOT_HEADER_VERSION := 2
+BOARD_INCLUDE_DTB_IN_BOOTIMG := true
+BOARD_KERNEL_BASE        := 0x00000000
+BOARD_KERNEL_CMDLINE += \
+    console=ttyMSM0,115200n8 androidboot.console=ttyMSM0 printk.devkmsg=on \
+    msm_rtb.filter=0x237 ehci-hcd.park=3 service_locator.enable=1 androidboot.memcg=1 \
+    cgroup.memory=nokmem usbcore.autosuspend=7 androidboot.usbcontroller=a600000.dwc3 swiotlb=2048 \
+    androidboot.boot_devices=soc/1d84000.ufshc loop.max_part=7 loop.hw_queue_depth=31 \
+    cgroup_disable=pressure
 BOARD_KERNEL_IMAGE_NAME := Image.lz4
+BOARD_KERNEL_PAGESIZE    := 4096
+BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 TARGET_COMPILE_WITH_MSM_KERNEL := true
 TARGET_KERNEL_CONFIG := floral_defconfig
 TARGET_KERNEL_SOURCE := kernel/google/msm-4.14
 TARGET_NEEDS_DTBOIMAGE := true
 
-# Reserve space for gapps install
+# Partitions
 -include vendor/lineage/config/BoardConfigReservedSize.mk
+BOARD_BOOTIMAGE_PARTITION_SIZE := 0x04000000
+BOARD_DTBOIMG_PARTITION_SIZE := 8388608
+BOARD_FLASH_BLOCK_SIZE := 131072
+BOARD_GOOGLE_DYNAMIC_PARTITIONS_PARTITION_LIST := \
+    product \
+    system \
+    system_ext \
+    vendor
+BOARD_GOOGLE_DYNAMIC_PARTITIONS_SIZE := 4873781248 # BOARD_SUPER_PARTITION_SIZE / 2 - 4MB
+BOARD_PERSISTIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_PERSISTIMAGE_PARTITION_SIZE := 33554432
+BOARD_PRODUCTIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_ROOT_EXTRA_SYMLINKS := /mnt/vendor/persist:/persist
+BOARD_SUPER_PARTITION_ERROR_LIMIT := 9651093504 # BOARD_SUPER_PARTITON_SIZE - 100MB
+BOARD_SUPER_PARTITION_GROUPS := google_dynamic_partitions
+BOARD_SUPER_PARTITION_SIZE := 9755951104
+BOARD_SYSTEM_EXTIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_USES_METADATA_PARTITION := true
+BOARD_USES_RECOVERY_AS_BOOT := true
+BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
+TARGET_USERIMAGES_USE_F2FS := true
+
+# Platform
+TARGET_BOARD_PLATFORM := msmnile
+
+# Recovery
+TARGET_RECOVERY_FSTAB := $(DEVICE_COMMON_PATH)/init/fstab.hardware
+TARGET_RECOVERY_PIXEL_FORMAT := RGBX_8888
+TARGET_RECOVERY_UI_LIB := \
+    //hardware/google/pixel/recovery:librecovery_ui_pixel \
+    libfstab
+TARGET_RECOVERY_WIPE := $(DEVICE_COMMON_PATH)/init/recovery.wipe
+
+# SELinux
+include $(DEVICE_COMMON_PATH)/sepolicy/coral-sepolicy.mk
+
+# Sound Trigger
+BOARD_SUPPORTS_SOUND_TRIGGER := true
+$(call soong_config_set,knowles,use_sound_trigger_hal,iaxxx)
+
+# Testing related defines
+BOARD_PERFSETUP_SCRIPT := platform_testing/scripts/perf-setup/c2f2-setup.sh
+
+# Verified Boot
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
+BOARD_AVB_VBMETA_SYSTEM := system system_ext product
+BOARD_AVB_VBMETA_SYSTEM_ALGORITHM := SHA256_RSA2048
+BOARD_AVB_VBMETA_SYSTEM_KEY_PATH := external/avb/test/data/testkey_rsa2048.pem
+BOARD_AVB_VBMETA_SYSTEM_ROLLBACK_INDEX := $(PLATFORM_SECURITY_PATCH_TIMESTAMP)
+BOARD_AVB_VBMETA_SYSTEM_ROLLBACK_INDEX_LOCATION := 1
+
+# Wi-Fi
+BOARD_WLAN_DEVICE := qcwcn # Ignored alphabetization, as other flags use it
+BOARD_HOSTAPD_DRIVER := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
+BOARD_WPA_SUPPLICANT_DRIVER := NL80211
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
+WIFI_DRIVER_DEFAULT := qca_cld3
+WIFI_FEATURE_IMU_DETECTION := true
+WIFI_FEATURE_WIFI_EXT_HAL := true
+WIFI_HIDL_FEATURE_AWARE := true
+WIFI_HIDL_FEATURE_DUAL_INTERFACE := true
+WIFI_HIDL_UNIFIED_SUPPLICANT_SERVICE_RC_ENTRY := true
+WPA_SUPPLICANT_VERSION := VER_0_8_X
