3f51ccd4b94afe2718b544e1fea68bf9338c56c2
Nolen Johnson <johnsonnolen@gmail.com>
2025-11-25 23:27:29 -0500

    floral: Drop incomplete automotive build support
    * We are missing the repo these makefiles live in.
    
    Change-Id: I751cffa53c3999ce2e2a8fd307f826fea664192b

--- flame/BoardConfig.mk
+++ flame/BoardConfig.mk
@@ -4,17 +4,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-ifdef AUTOMOTIVE_PRODUCT_PATH
-  #RBC# include_top device/google/auto_tcu
-  #RBC# include_top device/google/pixel_tcu
-  #RBC# include_top device/google_car
-  include device/$(AUTOMOTIVE_PRODUCT_PATH)/BoardConfig.mk
-else
-  TARGET_BOOTLOADER_BOARD_NAME := flame
-  TARGET_SCREEN_DENSITY := 440
-  TARGET_RECOVERY_UI_MARGIN_HEIGHT := 165
+TARGET_BOOTLOADER_BOARD_NAME := flame
+TARGET_SCREEN_DENSITY := 440
+TARGET_RECOVERY_UI_MARGIN_HEIGHT := 165
 
-  include device/google/coral/BoardConfig-common.mk
-endif
-
+include device/google/coral/BoardConfig-common.mk
 include vendor/google/flame/BoardConfigVendor.mk
