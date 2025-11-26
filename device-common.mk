8b48d9a482ecb3bd96423119c42680ff13624f62
Nolen Johnson <johnsonnolen@gmail.com>
2025-11-25 20:29:43 -0500

    floral: Kill unused utils.mk
    Change-Id: I361ad22e38469c7d8e6a378e7ee2c723d024f587

--- device-common.mk
+++ device-common.mk
@@ -47,8 +47,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 TARGET_PRODUCT_PROP := $(LOCAL_PATH)/product.prop
 
-$(call inherit-product, $(LOCAL_PATH)/utils.mk)
-
 # Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
 
