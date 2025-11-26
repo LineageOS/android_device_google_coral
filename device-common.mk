042f6a3977de7d49b74163e4dd6f6aa468686bd7
Nolen Johnson <johnsonnolen@gmail.com>
2025-11-26 02:38:10 -0500

    alpha
    Change-Id: I8048dec5821c62fedda78e67504b55b7623cfda6

--- device-common.mk
+++ device-common.mk
@@ -314,10 +314,10 @@ include device/google/gs-common/misc_writer/misc_writer.mk
 
 # NFC
 PRODUCT_PACKAGES += \
-    NfcOverlayCoral \
-    Tag \
     android.hardware.nfc-service.st \
-    android.hardware.secure_element@1.0-service.st
+    android.hardware.secure_element@1.0-service.st \
+    NfcOverlayCoral \
+    Tag
 
 PRODUCT_COPY_FILES += \
     device/google/coral/configs/nfc/com.google.hardware.pixel.japan.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/sku_G020N/com.google.hardware.pixel.japan.xml \
